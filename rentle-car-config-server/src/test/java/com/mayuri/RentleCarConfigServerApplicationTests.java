6807f501aefab936c271bf3649fd0ef518e47977
mayuritalati <39130231+mayuritalati@users.noreply.github.com>
2019-09-08 21:15:34 +0530

    Add files via upload
    config server

---
+++ rentle-car-config-server/src/test/java/com/mayuri/RentleCarConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentleCarConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
